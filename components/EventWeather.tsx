c4d2a68904ec6df1f658a5866581640c93fa224d
Brian Craig <briancraigok@gmail.com>
2020-08-03 16:52:14 -0300

    fix: tsx error

--- components/EventWeather.tsx
+++ components/EventWeather.tsx
@@ -1,15 +1,16 @@
+import React from 'react'
 import useFetch from "react-fetch-hook";
 import { Event } from "../models/event";
 
 export const WeatherStatus: React.FunctionComponent<{ event: Event }> = ({ event }) => {
   const { isLoading, data } = useFetch(`https://api.openweathermap.org/data/2.5/find?q=${event.location}&units=metric&appid=${process.env.NEXT_PUBLIC_OPENWEATHERMAP_KEY}`);
   if (isLoading) {
-    return "Loading..."
+    return <>{"Loading..."}</>
   }
   const matches: any[] = (data as any).list
   if (matches.length === 0){
-    return "City could not be found"
+    return <>{"City could not be found"}</>
   }
 
-  return `${matches[0].weather[0].description}, ${matches[0].main.temp}°C`
+  return <>{`${matches[0].weather[0].description}, ${matches[0].main.temp}°C`}</>
 }
